2fc58633d0b5e1cf07c004c2561210ce2405fa03
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-11-12 16:34:31 +0100

    Revert "Trimmed the branch to only the dummy_robot/dummy_robot_bringup sub directory"
    This reverts commit 48d882de63a456cf75d593c826bb46f4bac74523.

---
+++ lifecycle_py/setup.py
@@ -0,0 +1,46 @@
+import glob
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'lifecycle_py'
+
+setup(
+    name=package_name,
+    version='0.33.7',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/lifecycle_py/launch',
+            glob.glob(os.path.join('launch', '*_launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Ivan Santiago Paunovic',
+    author_email='ivanpauno@ekumenlabs.com',
+    maintainer='Aditya Pande, Audrow Nash, Michael Jeronimo',
+    maintainer_email='aditya.pande@openrobotics.org, audrow@openrobotics.org, michael.jeronimo@openrobotics.org',  # noqa: E501
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'Python lifecycle node demo'
+    ),
+    license='Apache License, Version 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'lifecycle_talker = lifecycle_py.talker:main',
+        ],
+    },
+)
